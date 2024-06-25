51f4ac014fec1b7fb1d71d8da1d8299994291cb3
Messiri4 <messirimiracle@gmail.com>
2024-06-25 12:33:13 +0100

    user registration

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,19 @@
 import User from "../models/User.js"
+import { StatusCodes } from "http-status-codes"
+import bcrypt from 'bcryptjs'
+import { hashPassword } from "../utils/passwordUtils.js"
+import { UnauthenticatedError } from "../errors/customErrors.js"
 
 export const register = async (req, res) => {
-    const user = new User(req.body)
-    try {
-        await user.save()
-        console.log(user);
-        res.status(201).send({user})
-    } catch (error) {
-        res.status(400).send(error)
-    }
+    // const {name, email, password} = req.body
+    //     const user = await User.create(req.body);
+    //     await user.save()
+    //     console.log(user);
+    //     res.status(201).json({user})
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await hashPassword(req.body.password)
+        req.body.password = hashedPassword
+
+        const user = await User.create(req.body)
+        res.status(StatusCodes.CREATED).json({msg: "user created"})
 }
--- routers/user.js
+++ routers/user.js
@@ -2,18 +2,9 @@ import express from "express";
 // import User from "../models/User.js";
 import { Router } from "express";
 import { register } from "../controllers/authController.js";
+import { validateRegisterInput } from "../middleware/validation.js";
 const router = Router();
 
-router.post("/register", register);
-
-// router.post("/users/login", async (req, res) => {
-//   try {
-//       const user = await User.findByCredentials(req.body.email, req.body.password);
-//     const token = await user.generateAuthToken();
-//     res.send({ user, token });
-//   } catch (error) {
-//     res.status(400).send(error);
-//   }
-// });
+router.post("/register", validateRegisterInput, register);
 
 export default router;
